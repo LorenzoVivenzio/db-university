fb9a1e78469fab9a7c335f466fa78da01127a23d
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2026-01-14 17:27:17 +0100

    feat: esercizi n5/6/7/8

--- database.sql
+++ database.sql
@@ -22,3 +22,29 @@ SELECT *
 FROM `courses`
 WHERE `period` = "I semestre"
 AND `year` = "1"
+
+
+
+-- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+SELECT *
+FROM `exams`
+WHERE `date` = "2020/06/20"
+AND `hour` > "14:00:00"
+
+
+
+-- Selezionare tutti i corsi di laurea magistrale
+SELECT *
+FROM `degrees`
+WHERE `level`= "magistrale"
+
+
+-- Da quanti dipartimenti è composta l'università?
+SELECT COUNT(*)
+FROM `departments`
+
+
+-- Quanti sono gli insegnanti che non hanno un numero di telefono?
+SELECT COUNT(*)
+FROM `teachers`
+WHERE `phone` IS NULL

dc07ef07c6f632bb3a747c9ec073672b7484a06d
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2026-01-14 16:55:25 +0100

    feat: esereczio n3

--- database.sql
+++ database.sql
@@ -8,3 +8,9 @@ WHERE YEAR (`date_of_birth`) = 1990
 SELECT *
 FROM `courses`
 WHERE `cfu` > 10
+
+-- STUDENTI DI ETA MAGGIORE A 30
+SELECT *
+FROM `students`
+WHERE YEAR (`date_of_birth`) <= 1995
+ORDER BY `date_of_birth`  DESC

4f2785b0c9d861910abd8ceac626cbe15389630b
Lorenzo Vivenzio <lorenzovivenzio988@hotmail.com>
2026-01-14 17:01:34 +0100

    feat: esercizio n4

--- database.sql
+++ database.sql
@@ -9,8 +9,16 @@ SELECT *
 FROM `courses`
 WHERE `cfu` > 10
 
+
 -- STUDENTI DI ETA MAGGIORE A 30
 SELECT *
 FROM `students`
 WHERE YEAR (`date_of_birth`) <= 1995
 ORDER BY `date_of_birth`  DESC
+
+
+-- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea 
+SELECT *
+FROM `courses`
+WHERE `period` = "I semestre"
+AND `year` = "1"
